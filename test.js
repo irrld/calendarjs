3d9cd522a4d68c8173d1a4696dcd4e1092087044
Metehan Gezer <irrelevantdev@outlook.com>
2022-05-25 19:13:28 +0000

    Example for calculating time until baker

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var {Calendar} = require('./calendar');
+var {
+    Calendar,
+    SEASON_WINTER,
+    SEASON_LENGTH
+} = require('./calendar');
 
 var calendar = new Calendar();
 // Mevsim ismi değiştirme
@@ -12,3 +16,15 @@ console.log(`SkyBlock Mevsimi: ${calendar.getCurrentSeasonName()}`)
 console.log(`SkyBlock Günü: ${calendar.getCurrentDay()}`)
 console.log(`SkyBlock Zamanı: ${calendar.getCurrentHour()}:${calendar.getFlatMinutes()}`)
 console.log(`Yağmur durumu ${calendar.isRaining()}`)
+if (calendar.getCurrentSeason() == SEASON_WINTER && calendar.getCurrentDay() > SEASON_LENGTH - 3) {
+    // Pastacı Kız burada
+} else {
+    var seasonsLeft = SEASON_WINTER - calendar.getCurrentSeason();
+    var day = calendar.getCurrentDay() - 1;
+    var timeUntilEvent = (seasonsLeft * SEASON_LENGTH * 20 * 60) + ((SEASON_LENGTH - day - 3) * 20 * 60) - (calendar.getEpochTime() % (20 * 60));
+    console.log(`Pastacı kızın gelmesine ${Math.floor(timeUntilEvent / 3600)} saat ${Math.floor(timeUntilEvent % 3600 / 60)} dakika kaldı.`)
+    /*
+    var s = calendar.internal.getCurrentSeason(calendar.getSkyBlockTime() + timeUntilEvent*72) // bu satır doğrulamak  
+    console.log(s)
+    */
+}
